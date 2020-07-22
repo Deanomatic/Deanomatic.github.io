0faa1429f9cec03f6e2ae90c1b45184d6ff948a0
Deanomatic <jonathdsmith19@gmail.com>
2020-07-22 18:08:59 -0500

    testing echo

--- form-test/form.php
+++ form-test/form.php
@@ -1,16 +1,16 @@
 <?php
 echo "<h2>TEST</h2>";
-if (isset($_POST['submit'])) {
-    $fullName = $_POST['fullName'];
-    $zipCode = $_POST['zipCode'];
-    $mailFrom = $_POST['email'];
-    $phoneNumber = $_POST['phoneNumber'];
+// if (isset($_POST['submit'])) {
+//     $fullName = $_POST['fullName'];
+//     $zipCode = $_POST['zipCode'];
+//     $mailFrom = $_POST['email'];
+//     $phoneNumber = $_POST['phoneNumber'];
 
-    $mailTo = "support@bigfootlawn.care";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have recieved an email from ".$fullName.".\n\n".$phoneNumber;
+//     $mailTo = "support@bigfootlawn.care";
+//     $headers = "From: ".$mailFrom;
+//     $txt = "You have recieved an email from ".$fullName.".\n\n".$phoneNumber;
 
-    mail($mailTo, $zipCode, $txt, $headers);
-    header("Location: indeax.php?mailsend");
-}
-?>
+//     mail($mailTo, $zipCode, $txt, $headers);
+//     header("Location: indeax.php?mailsend");
+// }
+// ?>
