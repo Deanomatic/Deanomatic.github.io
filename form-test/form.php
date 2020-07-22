a6ae7328e46ea86e894e806c73e59bbec0613063
Deanomatic <jonathdsmith19@gmail.com>
2020-07-22 17:53:29 -0500

    adding test form, and mnew styles

---
+++ form-test/form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $fullName = $_POST['fullName'];
+    $zipCode = $_POST['zipCode'];
+    $mailFrom = $_POST['email'];
+    $phoneNumber = $_POST['phoneNumber'];
+
+    $mailTo = "support@bigfootlawn.care";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have recieved an email from ".$fullName.".\n\n".$phoneNumber;
+
+    mail($mailTo, $zipCode, $txt, $headers);
+    header("Location: indeax.php?mailsend");
+}
+?>
