200f597819db0a69cafe1b4a711c4219d37259f7
Deanomatic <jonathdsmith19@gmail.com>
2021-02-10 20:29:10 -0600

    adding styling and script for animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-var string = "Hello, I am Dean Smith"; /* type your text here */
+var string = "Hello! My name is Dean, I am a Software Developer"; /* type your text here */
 var array = string.split("");
 var timer;
 
-function frameLooper () {
+function frameLooper() {
 	if (array.length > 0) {
 		document.getElementById("text").innerHTML += array.shift();
 	} else {
 		clearTimeout(timer);
-			}
-	loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+	}
+	loopTimer = setTimeout('frameLooper()', 70); /* change 70 for speed */
 
 }
 frameLooper();
